2b81a9fe787de9d82775e625c2700c6a1d32d74c
joao862 <146762759+joao862@users.noreply.github.com>
2024-12-28 22:41:58 +0000

    Update Blu.py

--- Blu.py
+++ Blu.py
@@ -178,12 +178,16 @@ elif page == "Worldwide Analysis":
 
     # Access the Streamlit secret containing the TOML-like service account info
     service_account_info = st.secrets
-    # Convert the Streamlit secrets to a JSON string
-    service_account_info_json = json.dumps(service_account_info, indent=4)
+    # Convert the Streamlit secrets to a regular dictionary
+    service_account_info_dict = dict(service_account_info)
+
+    # Convert the dictionary to a JSON string
+    service_account_info_json = json.dumps(service_account_info_dict, indent=4)
 
     # Display the JSON string in the app
     st.write("Service Account Info (JSON):")
     st.code(service_account_info_json, language="json")
+
     # File uploader for GeoJSON or KML
     uploaded_file = st.file_uploader("Upload a GeoJSON or KML File")
 
